8f17de06bd321bc8d20ea8628ee6bea33ef8fb51
RvnSytR <ravenskytd17@gmail.com>
2024-12-30 00:50:26 +0700

    Update column.tsx and data-table.tsx

--- components/layout/column.tsx
+++ components/layout/column.tsx
@@ -1,9 +1,10 @@
-// import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 
 import { type ButtonProps, Button } from "../ui/button";
 import { ArrowUpDown } from "lucide-react";
+import { UserCredentials } from "@/lib/db/schema";
 
-export const HeaderButton = ({ children, ...props }: ButtonProps) => {
+const HeaderButton = ({ children, ...props }: ButtonProps) => {
   return (
     <Button size="sm" variant="ghost" className="w-full" {...props}>
       {children}
@@ -11,3 +12,78 @@ export const HeaderButton = ({ children, ...props }: ButtonProps) => {
     </Button>
   );
 };
+
+export const colUser: ColumnDef<UserCredentials>[] = [
+  {
+    accessorKey: "num",
+    header: () => <div className="text-center">No</div>,
+    cell: ({ row }) => <div className="text-center">{row.index + 1}</div>,
+  },
+  {
+    accessorKey: "id_user",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        ID Pengguna
+      </HeaderButton>
+    ),
+    cell: ({ row }) => (
+      <div className="text-center">
+        {row.original.id_user.slice(0, 5) + "..."}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Email
+      </HeaderButton>
+    ),
+    cell: ({ row }) => <div className="text-center">{row.original.email}</div>,
+  },
+  {
+    accessorKey: "username",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Username
+      </HeaderButton>
+    ),
+    cell: ({ row }) => (
+      <div className="text-center">{row.original.username}</div>
+    ),
+  },
+  {
+    accessorKey: "last_signin_at",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Terakhir Login
+      </HeaderButton>
+    ),
+    cell: ({ row }) => (
+      <div className="text-center">
+        {row.original.last_signin_at.toUTCString()}
+      </div>
+    ),
+  },
+  {
+    accessorKey: "role",
+    header: ({ column }) => (
+      <HeaderButton
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Status
+      </HeaderButton>
+    ),
+    cell: ({ row }) => (
+      <div className="text-center capitalize">{row.original.role}</div>
+    ),
+  },
+];
--- components/layout/data-table.tsx
+++ components/layout/data-table.tsx
@@ -18,7 +18,7 @@ import type { UserCredentials } from "@/lib/db/schema";
 import { CreateUserDialog, DeleteUserDialog } from "./auth";
 import { SectionGroup, SectionTitle } from "./section";
 import { CustomButton } from "../global/custom-button";
-import { HeaderButton } from "./column";
+import { colUser } from "./column";
 
 import {
   Table,
@@ -149,80 +149,7 @@ export function AccountDataTable({
   const [globalFilter, setGlobalFilter] = useState<string>("");
 
   const columns: ColumnDef<UserCredentials>[] = [
-    {
-      accessorKey: "num",
-      header: () => <div className="text-center">No</div>,
-      cell: ({ row }) => <div className="text-center">{row.index + 1}</div>,
-    },
-    {
-      accessorKey: "id_user",
-      header: ({ column }) => (
-        <HeaderButton
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          ID Pengguna
-        </HeaderButton>
-      ),
-      cell: ({ row }) => (
-        <div className="text-center">
-          {row.original.id_user.slice(0, 5) + "..."}
-        </div>
-      ),
-    },
-    {
-      accessorKey: "email",
-      header: ({ column }) => (
-        <HeaderButton
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-        </HeaderButton>
-      ),
-      cell: ({ row }) => (
-        <div className="text-center">{row.original.email}</div>
-      ),
-    },
-    {
-      accessorKey: "username",
-      header: ({ column }) => (
-        <HeaderButton
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Username
-        </HeaderButton>
-      ),
-      cell: ({ row }) => (
-        <div className="text-center">{row.original.username}</div>
-      ),
-    },
-    {
-      accessorKey: "last_signin_at",
-      header: ({ column }) => (
-        <HeaderButton
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Terakhir Login
-        </HeaderButton>
-      ),
-      cell: ({ row }) => (
-        <div className="text-center">
-          {row.original.last_signin_at.toUTCString()}
-        </div>
-      ),
-    },
-    {
-      accessorKey: "role",
-      header: ({ column }) => (
-        <HeaderButton
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Status
-        </HeaderButton>
-      ),
-      cell: ({ row }) => (
-        <div className="text-center capitalize">{row.original.role}</div>
-      ),
-    },
+    ...colUser,
     {
       accessorKey: "action",
       header: () => <div className="text-center">Action</div>,
