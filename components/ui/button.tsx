a94a84647a5877597dc6afc2db00b26d0bc50492
RvnSytR <ravenskytd17@gmail.com>
2025-02-28 16:43:01 +0700

    add: button ghost variant | update: dropdown

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,24 +13,29 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         outline:
           "border border-input shadow-xs hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
 
         success:
           "bg-success text-success-foreground shadow-xs hover:bg-success/90  focus-visible:ring-success/20 dark:focus-visible:ring-success/40",
         outline_success:
           "border-success text-success border shadow-xs hover:bg-success hover:text-success-foreground focus-visible:border-success focus-visible:ring-success/20 dark:focus-visible:ring-success/40",
+        ghost_success:
+          "text-success hover:bg-success hover:text-success-foreground",
 
         warning:
           "bg-warning text-warning-foreground shadow-xs hover:bg-warning/90 focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40",
         outline_warning:
           "border-warning text-warning border shadow-xs hover:bg-warning hover:text-warning-foreground focus-visible:border-warning focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40",
+        ghost_warning:
+          "text-warning hover:bg-warning hover:text-warning-foreground",
 
         destructive:
           "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline_destructive:
           "border-destructive text-destructive border shadow-xs hover:bg-destructive hover:text-destructive-foreground focus-visible:border-destructive focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
-
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        ghost_destructive:
+          "text-destructive hover:bg-destructive hover:text-destructive-foreground",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
