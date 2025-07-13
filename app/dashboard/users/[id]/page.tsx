d819153569ad4f983290a569cad50fd37592e015
RvnSytR <ravenskytd17@gmail.com>
2025-07-13 11:33:05 +0700

    remove: [id] users

--- app/dashboard/users/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Section } from "@/components/layout/section";
-
-export default function Page() {
-  return (
-    <Section currentPage="Sum">
-      <p>Hello World</p>
-    </Section>
-  );
-}
